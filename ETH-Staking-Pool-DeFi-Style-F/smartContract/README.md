7b61085d9f68f63e274c4a6b725c5195807b93ba
YeisraelD <Yeisraeld@gmail.com>
2026-03-18 16:10:39 +0300

    forge init

--- ETH-Staking-Pool-DeFi-Style-F/smartContract/README.md
+++ ETH-Staking-Pool-DeFi-Style-F/smartContract/README.md
@@ -26,8 +26,6 @@ $ forge build
 ```shell
 $ forge test
 ```
-<img width="1625" height="283" alt="image" src="https://github.com/user-attachments/assets/5ca35787-4431-43db-aadf-3cb84b386729" />
-
 
 ### Format
 
--- ETH-Staking-Pool-DeFi-Style-F/smartContract/script/Counter.s.sol
+++ ETH-Staking-Pool-DeFi-Style-F/smartContract/script/Counter.s.sol
@@ -12,7 +12,7 @@ contract CounterScript is Script {
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter(1);
+        counter = new Counter();
 
         vm.stopBroadcast();
     }
