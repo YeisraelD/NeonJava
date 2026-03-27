d489f1c20171390431eb9dc39bd345a961ea875f
YeisraelD <Yeisraeld@gmail.com>
2026-03-28 00:00:58 +0300

    pending commits have been pushed

--- push_commits.ps1
+++
@@ -1,8 +0,0 @@
-$branch = (git branch --show-current).Trim()
-$commits = git rev-list --reverse "origin/$branch..HEAD"
-foreach ($c in $commits) {
-    Write-Host "Pushing commit: $c"
-    git push origin "$($c):$branch"
-    Start-Sleep -Seconds 2
-}
-Write-Host "All pending commits have been pushed!"
