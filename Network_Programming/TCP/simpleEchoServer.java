8327cd3b6e2b436048b6510f15ec0e315c4ce040
YeisraelD <Yeisraeld@gmail.com>
2026-04-17 22:31:26 +0300

    server listening to incomming connection

--- Network_Programming/TCP/simpleEchoServer.java
+++ Network_Programming/TCP/simpleEchoServer.java
@@ -8,6 +8,15 @@ public class SimpleEchoServer {
         int port = 5000; // below 1024 are reserved for system services
         System.out.println("[SERVER] Starting on port " + port);
 
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("[SERVER] waiting for a client....");// this stops the program and waitis a client to
+                                                                    // connect
+
+            try (Socket clienSocket = serverSocket.accept()) {
+                System.out.println("[SERVER] client connected! ");
+            }
+        }
+
     }
 
 }
