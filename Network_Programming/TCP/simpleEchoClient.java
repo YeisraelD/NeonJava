e7a5f21418b8cb788db8321932010a345967ebd5
YeisraelD <Yeisraeld@gmail.com>
2026-04-17 23:36:02 +0300

    handling exceptions

--- Network_Programming/TCP/simpleEchoClient.java
+++ Network_Programming/TCP/simpleEchoClient.java
@@ -15,8 +15,23 @@ public class SimpleEchoClient {
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));//recive data from the server
 
             Scanner scanner = new Scanner(System.in);
-            System.out.print("[CLIENT] Connected type a message and exit to quite: ")
+            System.out.println("[CLIENT] Connected type a message and exit to quite: ");
+
+            while(true){
+                System.out.print("[CLiENT] message: ");
+                String clientMessage = scanner.nextLine();
+                out.println(clientMessage); // send it
+                String response = in.readLine;
+
+                System.out.println("[client] server response: " + response);
+
+                if ("exit".equalsIgnoreCase(clientMessage)){
+                    break;
+                }
+            }
             
+        } catch (IOException e){
+            System.err.println("client error: " + e.getMessage());
         }
 
 
