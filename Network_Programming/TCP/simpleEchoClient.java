2efdedb35535ad0e5e637cf0f0f214d2e0737717
YeisraelD <Yeisraeld@gmail.com>
2026-04-17 23:23:38 +0300

    connection and streaming, only direct socket

--- Network_Programming/TCP/simpleEchoClient.java
+++ Network_Programming/TCP/simpleEchoClient.java
@@ -10,6 +10,15 @@ public class SimpleEchoClient {
         int port = 5000;
 
         Syetem.out.println("[CLIENT] connecting to: " + host + ": " + port);
+        try(Socket socket = new socket(host, port)){
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true); //send daat to the server
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));//recive data from the server
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("[CLIENT] Connected type a message and exit to quite: ")
+            
+        }
+
 
     }
 }
